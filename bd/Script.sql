adb4cb446de0c4f3011c88af71fd8072d659849d
Luan <luan.magno@sptech.school>
2023-05-16 20:04:56 -0300

    Inicio pagina teste e ajuste no login e cadastro

---
+++ bd/Script.sql
@@ -0,0 +1,19 @@
+create database RE2;
+drop database RE2;
+
+use RE2;
+create table usuario(
+idUsuario int auto_increment primary key,
+email varchar(45),
+senha int,
+dtNac date
+);
+
+create table tentativas(
+idTentativas int auto_increment primary key,
+email varchar(45),
+TotalPontos int,
+fkUsuario int,
+constraint fkUsuarioTentativas foreign key (fkUsuario) references usuario(idUsuario)
+);
+
