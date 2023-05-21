b751afbb82b7f88d8c0bd342402186ddb5a2400a
Luan <luan.magno@sptech.school>
2023-05-21 19:26:37 -0300

    atualização da pg teste no grafico

--- bd/Script.sql
+++ bd/Script.sql
@@ -5,13 +5,12 @@ use RE2;
 create table usuario(
 idUsuario int auto_increment primary key,
 email varchar(45),
-senha int,
-dtNac date
+senha int
 );
 
 create table tentativas(
 idTentativas int auto_increment primary key,
-email varchar(45),
+email varchar(45) unique,
 TotalPontos int,
 fkUsuario int,
 constraint fkUsuarioTentativas foreign key (fkUsuario) references usuario(idUsuario)
