c30c713f7b10aefd4fe13c34ec39879847a58bce
Luan <luan.magno@sptech.school>
2023-06-06 20:08:42 -0300

    integracao api...

--- bd/Script.sql
+++ bd/Script.sql
@@ -4,16 +4,17 @@ drop database RE2;
 use RE2;
 create table usuario(
 idUsuario int auto_increment primary key,
-email varchar(45),
+email varchar(45) unique,
 senha int
-);
+); drop table usuario;
 
 create table tentativas(
-idTentativas int auto_increment primary key,
+idTentativas int auto_increment,
 email varchar(45) unique,
 TotalPontos int,
 fkUsuario int,
 constraint fkUsuarioTentativas foreign key (fkUsuario) references usuario(idUsuario),
 constraint pkComposta primary key (idTentativas, fkUsuario)
-);
+); drop table tentativas;
+desc tentativas;
 
